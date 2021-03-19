8160efa9f319f58ba3392d1bbd92207de995bc7e
Jane Kotovich <jenyakotovich@gmail.com>
2021-03-19 15:52:38 +1000

    joint table formed on index page

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,12 @@ app.use(
 //TO_CHAR(schedules.end_at, 'HH:MI am') as end_at
 
 // const queryDisplayCombined = `SELECT users.surname, schedules.unique_key, users.firstname, schedules.week_day, TO_CHAR(schedules.start_at, 'HH:MI am') AS start_at, TO_CHAR(schedules.end_at, 'HH:MI am') as end_at, TO_CHAR(schedules.end_at, 'DD-Month-YYYY') as date FROM users JOIN schedules ON schedules.user_id=users.user_id WHERE users.user_id='${userId}' ORDER BY schedules.end_at;`;
-// `SELECT list_to_do.item_to_do, users.firstname, users.secondname,list_to_do.done, TO_CHAR(list_to_do.item_to_do.created_at, 'DD-Month-YYYY') as date_created, TO_CHAR(list_to_do.item_to_do.created_at, 'HH:MI am') AS time_created,TO_CHAR(list_to_do.item_to_do.updated_at, 'DD-Month-YYYY') AS date_done, TO_CHAR(list_to_do.item_to_do.updated_at, 'HH:MI am') AS time_done FROM users JOIN list_to_do ON list_to_do.user_id=users.user_id ;`
+// `SELECT list_to_do.item_to_do, users.firstname, users.secondname,list_to_do.done, TO_CHAR(list_to_do.created_at, 'DD-Month-YYYY') as date_created, TO_CHAR(list_to_do.created_at, 'HH:MI am') AS time_created,TO_CHAR(list_to_do.updated_at, 'DD-Month-YYYY') AS date_done, TO_CHAR(list_to_do.updated_at, 'HH:MI am') AS time_done FROM users JOIN list_to_do ON list_to_do.user_id=users.user_id ;`
 
 router.get("/", (req, res) => {
   pool.query(
-    `SELECT item_to_do, done, TO_CHAR(created_at, 'DD-Month-YYYY') as date_created, TO_CHAR(created_at, 'HH:MI am') AS time_created,TO_CHAR(updated_at, 'DD-Month-YYYY') AS date_done, TO_CHAR(updated_at, 'HH:MI am') AS time_done FROM list_to_do;`,
+    `SELECT list_to_do.item_to_do, users.firstname, users.secondname,list_to_do.done, TO_CHAR(list_to_do.created_at, 'DD-Month-YYYY') as date_created, TO_CHAR(list_to_do.created_at, 'HH:MI am') AS time_created,TO_CHAR(list_to_do.updated_at, 'DD-Month-YYYY') AS date_done, TO_CHAR(list_to_do.updated_at, 'HH:MI am') AS time_done FROM users JOIN list_to_do ON list_to_do.user_id=users.user_id ;`,
+
     (err, result) => {
       if (err) {
         throw err;
