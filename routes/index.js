23926621d8495ff6a6b24070b14216c7c0fb7c19
Jane Kotovich <jenyakotovich@gmail.com>
2021-03-19 14:25:32 +1000

    Working on showing list of does in index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 /* Call required package modules */
 const express = require("express");
 const router = express.Router();
+const session = require("express-session");
+/* Call database */
+const { pool } = require("../database.js");
 const app = express();
+//ssessions
+app.use(
+  session({
+    resave: false,
+    secret: "shh/its1asecret",
+    saveUninitialized: false,
+    //secure:false
+  })
+);
 
 router.get("/", (req, res) => {
-  res.render("pages/index");
+  pool.query(`SELECT * FROM list_to_do;`, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(result.rows);
+      res.render("pages/index");
+    }
+  });
 });
 
 /* Export router to app.js */
