09966a8e449e5805ab04ed3d84f45a0bb6311cb0
Jane Kotovich <jenyakotovich@gmail.com>
2021-03-19 15:41:07 +1000

    Added do-list in index page

--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,26 @@ app.use(
     //secure:false
   })
 );
+//TO_CHAR(schedules.end_at, 'HH:MI am') as end_at
+
+// const queryDisplayCombined = `SELECT users.surname, schedules.unique_key, users.firstname, schedules.week_day, TO_CHAR(schedules.start_at, 'HH:MI am') AS start_at, TO_CHAR(schedules.end_at, 'HH:MI am') as end_at, TO_CHAR(schedules.end_at, 'DD-Month-YYYY') as date FROM users JOIN schedules ON schedules.user_id=users.user_id WHERE users.user_id='${userId}' ORDER BY schedules.end_at;`;
+// `SELECT list_to_do.item_to_do, users.firstname, users.secondname,list_to_do.done, TO_CHAR(list_to_do.item_to_do.created_at, 'DD-Month-YYYY') as date_created, TO_CHAR(list_to_do.item_to_do.created_at, 'HH:MI am') AS time_created,TO_CHAR(list_to_do.item_to_do.updated_at, 'DD-Month-YYYY') AS date_done, TO_CHAR(list_to_do.item_to_do.updated_at, 'HH:MI am') AS time_done FROM users JOIN list_to_do ON list_to_do.user_id=users.user_id ;`
 
 router.get("/", (req, res) => {
-  pool.query(`SELECT * FROM list_to_do;`, (err, result) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(result.rows);
-      res.render("pages/index");
+  pool.query(
+    `SELECT item_to_do, done, TO_CHAR(created_at, 'DD-Month-YYYY') as date_created, TO_CHAR(created_at, 'HH:MI am') AS time_created,TO_CHAR(updated_at, 'DD-Month-YYYY') AS date_done, TO_CHAR(updated_at, 'HH:MI am') AS time_done FROM list_to_do;`,
+    (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(result.rows);
+        const allListToDo = result.rows;
+        res.render("pages/index", {
+          allListToDo: allListToDo,
+        });
+      }
     }
-  });
+  );
 });
 
 /* Export router to app.js */
